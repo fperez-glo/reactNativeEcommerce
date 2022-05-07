b3765e6140e71735bd0f2c06ecc8c8ae9f46bc2f
fperez-glo <facundoperez6@gmail.com>
2022-05-06 21:13:54 -0300

    branch al repo

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity
+        style={styles.button}>
+        <Text>Aceptar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +21,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button:{
+    backgroundColor: "lightblue",
+    width:100,
+    height:40,
+    borderRadius: 25,
+    padding:10,
+    justifyContent:"center",
+    alignItems:"center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 2.5,
+    elevation: 3,
+  }
 });
